c22a7b8f174a298b48c736b549590578fe203f54
Brod-web <frederic.brodut@gmail.com>
2021-04-28 15:51:59 +0200

    Creation Activity + xml

--- app/src/main/java/com/example/app_copains_ecole/MainActivity.kt
+++ app/src/main/java/com/example/app_copains_ecole/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.app_copains_ecole
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onBtnRegisterClick(view: View) {
+        val intent = Intent(this, RegisterActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun onBtnLoginClick(view: View) {
+        val intent = Intent(this, MapsActivity::class.java)
+        startActivity(intent)
     }
 }
